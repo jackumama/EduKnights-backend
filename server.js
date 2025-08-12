dc49a9da43838e225fffe3edd38b0546fa26d88c
jackumama <jackblog0018@gmail.com>
2025-08-12 21:15:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 // Use Render's assigned PORT or fallback for local dev
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(🚀 Server running on port ${PORT});
 console.log(`Server running on port ${PORT}`);
 });
+
 
